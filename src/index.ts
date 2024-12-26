b4d7566e4b03bea5fe76f8191e1cf530b1785a68
Jake Holland <contact@hollandjake.com>
2024-12-26 08:27:42 +0000

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { ExtendedPDFDocument as default } from './document';
+export type { PDFPage } from './page';
+export type { PDFTable, PDFTableOpts } from './table';
+export type { Cell, ExtendedPDFDocumentOptions, Size, Wideness } from './types';
